7c7ff39d9ee32a60cdc069dc1efd8b3285393bae
Oleksandr Human <oleksnadrguman@gmail.com>
2023-04-20 10:02:09 +0300

    Made the server run forever and refactored

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const io = require("socket.io")(server, { 
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
   cors: {
-    origin: "*"
-  }
+    origin: "*",
+  },
 });
 
 const port = process.env.PORT || 8080; // create port relative the host or just localhost:8080
@@ -54,7 +54,6 @@ const setStepTimer = (me, room) => {
     }, 1000),
   });
 };
-
 // when anyone has won the battle
 const onWin = (winnerCells, whoWill, step, room, corner) => {
   io.to(room).emit("blocked"); // deny players to step
@@ -65,17 +64,19 @@ const onWin = (winnerCells, whoWill, step, room, corner) => {
 
 // when player disconnected from room
 const disconnected = (playerRoom, room, opponentRoom) => {
-  if(toMenuIntervals?.get(room)?.func===undefined){ // checking is there already menu timer to avoid showing disconnect modal with another modal
+  if (toMenuIntervals?.get(room)?.func === undefined) {
+    // checking is there already menu timer to avoid showing disconnect modal with another modal
     io.to(room).emit("disconnected"); // send to room that opponent left
     setToMenuTimer(room, 10); // set timer to send player to menu
-    try { // does this player has timer in general?
+    try {
+      // does this player has timer in general?
       clearInterval(stepIntervals.get(playerRoom).func);
-    } 
-    catch { // if not
-      try { // does this player has timer in general too?
+    } catch {
+      // if not
+      try {
+        // does this player has timer in general too?
         clearInterval(stepIntervals.get(opponentRoom).func);
-      } 
-      catch {}
+      } catch {}
     }
   }
 };
@@ -90,9 +91,11 @@ let cellsNumber = cellsInLine * cellsInLine;
 io.on("connection", (socket) => {
   socket.join("im online"); // this room is for number of online players
   io.to("in waiting").emit("send online", roomLength("im online")); // send number of players inside room "im online" to waiting players
-  socket.on("im waiting", () => { // when player have rendered the component
+  socket.on("im waiting", () => {
+    // when player have rendered the component
     socket.join("in waiting"); // send player to waiting room
-    if (roomLength("in waiting") > 1) { // when there are 2 players or more
+    if (roomLength("in waiting") > 1) {
+      // when there are 2 players or more
       // generating an unique room
       roomIndex++;
       let currentRoom = `room ${roomIndex}`;
@@ -120,36 +123,46 @@ io.on("connection", (socket) => {
       io.to(playerX).emit("unblocked"); // let player to make step
     }
   });
-  socket.on("disconnect", () => { // when any player disconnected
+  socket.on("disconnect", () => {
+    // when any player disconnected
     io.to("in waiting").emit("send online", roomLength("im online")); //send number of online players to waiting room
   });
-  socket.on("im leaving", (playerRoom, room, opponentRoom) => { // when player left the game
+  socket.on("im leaving", (playerRoom, room, opponentRoom) => {
+    // when player left the game
     socket.leave(room);
     socket.leave(playerRoom);
     socket.leave("in waiting");
     disconnected(playerRoom, room, opponentRoom);
   });
-  socket.on("cellClick", (cells, id, room, step, whoClicked, whoWill) => { // when clicked any cell
-    if (cells[id] === "") { // is the cell empty?
+  socket.on("cellClick", (cells, id, room, step, whoClicked, whoWill) => {
+    // when clicked any cell
+    if (cells[id] === "") {
+      // is the cell empty?
       let field = cells;
       let winnerCells = [];
       field[id] = step; // setting cell that clicked
       clearInterval(stepIntervals.get(whoClicked).func); // removing timer from player that has clicked
       setStepTimer(whoWill, room); // setting timer another player
       io.to(room).emit("fieldReload", field); // sending updated field to players
-      io.to(whoClicked).emit("blocked"); // deny player that has clicked to step 
-      io.to(whoWill).emit("unblocked"); // let player that will click to step 
-      cellsCheck: for (let k = 0; k <= 4; k++) { // checking cells for a winner or when it's draw (using loop, to break it when winner found or it's draw)
-        if (k === 0) { // horizontal checking
+      io.to(whoClicked).emit("blocked"); // deny player that has clicked to step
+      io.to(whoWill).emit("unblocked"); // let player that will click to step
+      cellsCheck: for (let k = 0; k <= 4; k++) {
+        // checking cells for a winner or when it's draw (using loop, to break it when winner found or it's draw)
+        if (k === 0) {
+          // horizontal checking
           let stepsInLine;
-          for (let k = 0; k < cellsNumber; k += cellsInLine) { // checking every horizontal line
+          for (let k = 0; k < cellsNumber; k += cellsInLine) {
+            // checking every horizontal line
             stepsInLine = 0;
             winnerCells = [];
-            for (j = k; j < k + cellsInLine; j++) { // checking every cell in the horizontal line
-              if (cells[j] === step) { // if cell value = step
+            for (j = k; j < k + cellsInLine; j++) {
+              // checking every cell in the horizontal line
+              if (cells[j] === step) {
+                // if cell value = step
                 stepsInLine++; // increment number of cells whit this value
                 winnerCells.push(j); // push id to an array to make cross this cells
-                if (stepsInLine === cellsInLine) { // if each line's cell has step value
+                if (stepsInLine === cellsInLine) {
+                  // if each line's cell has step value
                   onWin(winnerCells, whoWill, step, room, "0deg"); // set winner
                   break cellsCheck; // break loop
                 }
@@ -157,16 +170,21 @@ io.on("connection", (socket) => {
             }
           }
         }
-        if (k === 1) { // vertical checking
+        if (k === 1) {
+          // vertical checking
           let stepsInLine;
-          for (let k = 0; k < cellsInLine; k++) { // checking every vertical line
+          for (let k = 0; k < cellsInLine; k++) {
+            // checking every vertical line
             stepsInLine = 0;
             winnerCells = [];
-            for (j = k; j <= cellsNumber; j += cellsInLine) { // checking every cell in the vertical line
-              if (cells[j] === step) { // if cell value = step
+            for (j = k; j <= cellsNumber; j += cellsInLine) {
+              // checking every cell in the vertical line
+              if (cells[j] === step) {
+                // if cell value = step
                 stepsInLine++; // increment number of cells whit this value
                 winnerCells.push(j); // push id to an array to make cross this cells
-                if (stepsInLine === cellsInLine) { // if each line's cell has step value
+                if (stepsInLine === cellsInLine) {
+                  // if each line's cell has step value
                   onWin(winnerCells, whoWill, step, room, "90deg"); // set winner
                   break cellsCheck; // break loop
                 }
@@ -174,14 +192,18 @@ io.on("connection", (socket) => {
             }
           }
         }
-        if (k === 2) { // diagonal checking from left top
+        if (k === 2) {
+          // diagonal checking from left top
           let stepsInLine = 0;
           winnerCells = [];
-          for (let k = 0; k < cellsNumber; ) { // checking diagonal line from left top to right bottom (\)
-            if (cells[k] === step) { // if cell value = step
+          for (let k = 0; k < cellsNumber; ) {
+            // checking diagonal line from left top to right bottom (\)
+            if (cells[k] === step) {
+              // if cell value = step
               stepsInLine++; // increment number of cells whit this value
               winnerCells.push(k); // push id to an array to make cross this cells
-              if (stepsInLine === cellsInLine) { // if each line's cell has step value
+              if (stepsInLine === cellsInLine) {
+                // if each line's cell has step value
                 onWin(winnerCells, whoWill, step, room, "45deg"); // set winner
                 break cellsCheck; // break loop
               }
@@ -189,14 +211,18 @@ io.on("connection", (socket) => {
             k = k + cellsInLine + 1;
           }
         }
-        if (k === 3) { // diagonal checking from left bottom
+        if (k === 3) {
+          // diagonal checking from left bottom
           let stepsInLine = 0;
           winnerCells = [];
-          for (let k = cellsInLine - 1; k <= cellsNumber - 2; ) { // checking diagonal line from left bottom to right top (/)
-            if (cells[k] === step) { // if cell value = step
+          for (let k = cellsInLine - 1; k <= cellsNumber - 2; ) {
+            // checking diagonal line from left bottom to right top (/)
+            if (cells[k] === step) {
+              // if cell value = step
               stepsInLine++; // increment number of cells whit this value
               winnerCells.push(k); // push id to an array to make cross this cells
-              if (stepsInLine === cellsInLine) { // if each line's cell has step value
+              if (stepsInLine === cellsInLine) {
+                // if each line's cell has step value
                 onWin(winnerCells, whoWill, step, room, "135deg"); // set winner
                 break cellsCheck; // break loop
               }
@@ -204,13 +230,17 @@ io.on("connection", (socket) => {
             k = k + cellsInLine - 1;
           }
         }
-        if (k === 4) { // checking for a draw
+        if (k === 4) {
+          // checking for a draw
           let busyCells = 0;
-          for (j = 0; j <= cellsInLine * cellsInLine; j++) { // checking each cell of field
-            if (cells[j] === "x" || cells[j] === "o") { // if cell has any value
+          for (j = 0; j <= cellsInLine * cellsInLine; j++) {
+            // checking each cell of field
+            if (cells[j] === "x" || cells[j] === "o") {
+              // if cell has any value
               busyCells++; // increase the number of busy cells
             }
-            if (busyCells === 9) { // if busy cells = 9
+            if (busyCells === 9) {
+              // if busy cells = 9
               winnerFound = true; // winner = draw
               io.to(room).emit("blocked"); // deny players to step
               io.to(room).emit("win", [], "", "draw"); // send to players that it's draw
@@ -223,14 +253,17 @@ io.on("connection", (socket) => {
       }
     }
   });
-  socket.on("play again", (room, againVotes, myRoom) => { // when anyone clicked to play again 
+  socket.on("play again", (room, againVotes, myRoom) => {
+    // when anyone clicked to play again
     let againVotesIncremented = againVotes;
-    if (againVotes < 2) { // if votes < 2
+    if (againVotes < 2) {
+      // if votes < 2
       againVotesIncremented++;
       io.to(myRoom).emit("vote btn blocked"); // player that voted can't step more
       io.to(room).emit("votes reload", againVotesIncremented); // sending updated votes to players
       io.to(myRoom).emit("blocked");
-      if (againVotesIncremented === 2) { // when votes = 2
+      if (againVotesIncremented === 2) {
+        // when votes = 2
         clearInterval(toMenuIntervals.get(room).func); // timer to send players to menu stopped
         toMenuIntervals.get(room).func = undefined;
         io.to(room).emit("restart"); // restart the game
@@ -238,10 +271,12 @@ io.on("connection", (socket) => {
       }
     }
   });
-  socket.on("send name", (myName, opponentRoom) => { // when player sending his name
+  socket.on("send name", (myName, opponentRoom) => {
+    // when player sending his name
     io.to(opponentRoom).emit("get opponent name", myName); // send to opponent his name
   });
-  socket.on("send message", (inputValue, myRoom, opponentRoom) => { // when player sending message
+  socket.on("send message", (inputValue, myRoom, opponentRoom) => {
+    // when player sending message
     const now = new Date(); // current date
     // current time
     const current =
@@ -250,20 +285,42 @@ io.on("connection", (socket) => {
       ":" +
       (now.getMinutes() < 10 ? "0" : "") +
       now.getMinutes();
-    io.to(myRoom).emit("get message", { // send to player that send message it as his
+    io.to(myRoom).emit("get message", {
+      // send to player that send message it as his
       text: inputValue,
       time: current,
       whose: "my",
     });
-    io.to(opponentRoom).emit("get message", { // send to player that send message it as opponent's
+    io.to(opponentRoom).emit("get message", {
+      // send to player that send message it as opponent's
       text: inputValue,
       time: current,
       whose: "opponent",
     });
   });
-  socket.on("get online", () => { // when player requests number of players online
+  socket.on("get online", () => {
+    // when player requests number of players online
     io.to("in waiting").emit("send online", roomLength("im online")); // send number of players to player
   });
 });
 
+/* This part of the code is keeping the server running forever. 
+The hosting is turning the server off when the script isn't used anywhere by its Express url for about 30 minutes. 
+Forever node.js package isn't working on this hosting. 
+The best option is to send request with GET method for this script from itself
+*/
+const https = require("https");
+
+const options = {
+  hostname: process.env.HOSTNAME,
+  method: "GET",
+};
+
+setInterval(() => {
+  const req = https.request(options);
+
+  req.end();
+}, 30000);
+
+// Listening for incoming requests on the specified port
 server.listen(port);
